76f08842b6b9970f1ca091b7e842e4561286e0ce
Kevin Pierce <kpierce236@gmail.com>
2023-11-12 15:28:45 -0500

    added function that fetches the ccity weather data

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,6 @@ var APIkey = "ab74206522f65109633d42c234243129";
 //fetch lat and long of city
 var getCoords = function () {
     var city = $('input').val();
-    console.log(encodeURIComponent(city));
-    console.log(city);
 
     var apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q='+encodeURIComponent(city)+'&appid='+APIkey;
 
@@ -14,6 +12,7 @@ var getCoords = function () {
         if (response.ok) {
           response.json().then(function (data) {
             console.log(data);
+            getCityWeather(data);
           });
         } else {
           alert('Error: ' + response.statusText);
@@ -23,3 +22,34 @@ var getCoords = function () {
 
 searchBtn.click(getCoords);
 //fetch weather data 
+var getCityWeather =  function (data) {
+ var lat= data[0].lat;
+ var lon = data[0].lon;
+
+ var currenUrl= "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+APIkey;
+ var fiveDayUrl= "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid="+APIkey;
+
+ fetch(currenUrl)
+      .then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+            console.log(data);
+          });
+        } else {
+          alert('Error: ' + response.statusText);
+        }
+      })
+
+ fetch(fiveDayUrl)
+      .then(function (response) {
+        if(response.ok) {
+          response.json().then(function (data) {
+            console.log(data);
+          });
+        } else {
+          alert('Error: ' + response.statusText);
+        }
+      })
+
+
+};
